95c5abf7cf9c7412653cdc73a4252b0ff3981f67
kosnire <kosnire@gmail.com>
2018-01-09 15:44:32 -0500

    Added credentials template for easy uploads to Azure.

---
+++ credentials/template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###
+# This file makes it easy to set bash env vars that allow uploads to Azure under your account.
+# Copy this template file and name it <your-account>.sh
+# See https://serverless.com/framework/docs/providers/azure/guide/credentials/
+#   for how to fill in the variables.
+# Then just run `source credentials/<your-account>.sh` and you'll be all set to upload to Azure.
+###
+
+export azureSubId=
+export azureServicePrincipalTenantId=
+export azureServicePrincipalClientId=
+export azureServicePrincipalPassword=
+
+# Turn SLS debug logging on (optional)
+export SLS_DEBUG=true
+
+echo "Azure env vars set for <your-account>"
