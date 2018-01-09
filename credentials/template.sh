d40ab4d954eaa8a40ce18c80c10a4d0e68a3845c
kosnire <kosnire@gmail.com>
2018-01-09 17:19:13 -0500

    Small clarifications in credentials template.

--- credentials/template.sh
+++ credentials/template.sh
@@ -5,13 +5,14 @@
 # Copy this template file and name it <your-account>.sh
 # See https://serverless.com/framework/docs/providers/azure/guide/credentials/
 #   for how to fill in the variables.
-# Then just run `source credentials/<your-account>.sh` and you'll be all set to upload to Azure.
+# Then just run `source credentials/<your-account>.sh` and you'll be all set to run sls deploy
+#   and upload to Azure as many times as you want!
 ###
 
-export azureSubId=
-export azureServicePrincipalTenantId=
-export azureServicePrincipalClientId=
-export azureServicePrincipalPassword=
+export azureSubId=''
+export azureServicePrincipalTenantId=''
+export azureServicePrincipalClientId=''
+export azureServicePrincipalPassword=''
 
 # Turn SLS debug logging on (optional)
 export SLS_DEBUG=true
